c0c30d19eb2c675e455a0079c66580d9f466356e
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-05-27 11:43:13 +0300

    create base fragment, adapter and viewModel

---
+++ app/src/main/java/com/example/swiftshop/ui/base/BaseFragment.kt
@@ -0,0 +1,34 @@
+package com.example.swiftshop.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import com.example.swiftshop.BR
+
+abstract class BaseFragment<VDB : ViewDataBinding, VM : BaseViewModel> : Fragment() {
+
+    private var _binding: VDB? = null
+    protected val binding: VDB
+        get() = _binding as VDB
+
+    abstract val viewModel: VM
+
+    abstract val layoutIdFragment: Int
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutIdFragment, container, false)
+        _binding?.apply {
+            lifecycleOwner = viewLifecycleOwner
+            setVariable(BR.viewModel, viewModel)
+        }
+        return binding.root
+    }
+}
