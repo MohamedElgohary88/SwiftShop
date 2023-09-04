eebdc74202db3bb519353a15c3850cfae10a24d3
Mohamed Elgohary <mohamed.abdelazim.elgohary@gmail.com>
2023-09-04 14:30:05 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SwiftShop"
+include(":app")
+include(":ui")
+include(":remote")
+include(":local")
+include(":repository")
+include(":viewmodels")
+include(":usecases")
+include(":entities")
